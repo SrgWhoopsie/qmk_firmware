3e49ead896eca8d91e5667922ade131837112088
SrgWhoopsie <107329142+SrgWhoopsie@users.noreply.github.com>
2023-11-05 12:25:54 +1100

    reset to default qmk firmware
    default dumbpad v1x_oled from QMK repo. currently only oled works, keys do not work as intended.

--- keyboards/dumbpad/v1x_oled/keymaps/via/keymap.c
+++ keyboards/dumbpad/v1x_oled/keymaps/via/keymap.c
@@ -14,22 +14,10 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
-#include <stdio.h>
-char wpm_str[10];
-
-// Each layer gets a name for readability, which is then used in the keymap matrix below.
-// The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
-enum layers {
-    _BL,
-    _CAD,
-    _FL
-};
-
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /* Keymap _BL: (Base Layer) Default Layer
+    /*
+            BASE LAYER
     /-----------------------------------------------------`
     |             |    7    |    8    |    9    |  Bkspc  |
     |             |---------|---------|---------|---------|
@@ -40,13 +28,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     | Play Pause  |  TT(1)  |    0    |    .    |  Enter  |
     \-----------------------------------------------------'
     */
-    [1] = LAYOUT(
+    [0] = LAYOUT(
                     KC_7,      KC_8,    KC_9,             KC_BSPC,
                     KC_4,      KC_5,    KC_6,             KC_ESC,
                     KC_1,      KC_2,    KC_3,             KC_TAB,
-        KC_MPLY,    TT(1),     KC_0,    LSFT_T(KC_DOT),   KC_ENTER
+        KC_MUTE,    TT(1),     KC_0,    LSFT_T(KC_DOT),   KC_ENTER
     ),
-    /* Keymap _CAD: FreeCad layer
+    /*
+            SUB LAYER
     /-----------------------------------------------------`
     |             |         |         |         |  Reset  |
     |             |---------|---------|---------|---------|
@@ -57,30 +46,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     |    LOCK     |         |         |         |    =    |
     \-----------------------------------------------------'
     */
-    [2] = LAYOUT(
+    [1] = LAYOUT(
                     _______,     _______,     _______,      QK_BOOT,
                     _______,     _______,     _______,      KC_KP_PLUS,
                     _______,     _______,     _______,      KC_KP_MINUS,
         QK_LOCK,    _______,     _______,     _______,      KC_EQL
     ),
-    /* Keymap _FL: FL Studio layer
-    /-----------------------------------------------------`
-    |             |         |         |         |  Reset  |
-    |             |---------|---------|---------|---------|
-    |             |         |         |         |    +    |
-    |             |---------|---------|---------|---------|
-    |             |         |         |         |    -    |
-    |-------------|---------|---------|---------|---------|
-    |    LOCK     |         |         |         |    =    |
-    \-----------------------------------------------------'
 
-    [3] = LAYOUT(
-                    _______,     _______,     _______,      QK_BOOT,
-                    _______,     _______,     _______,      KC_KP_PLUS,
-                    _______,     _______,     _______,      KC_KP_MINUS,
-        QK_LOCK,    _______,     _______,     _______,      KC_EQL
+    [2] = LAYOUT(
+                    _______,     _______,     _______,      _______,
+                    _______,     _______,     _______,      _______,
+                    _______,     _______,     _______,      _______,
+        _______,    _______,     _______,     _______,      _______
     ),
-    */
+
     [3] = LAYOUT(
                     _______,     _______,     _______,      _______,
                     _______,     _______,     _______,      _______,
@@ -88,4 +67,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,    _______,     _______,     _______,      _______
     )
 };
-
